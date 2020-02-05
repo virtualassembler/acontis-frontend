8a59dbcf0ad05fdd192db1dec465cda91136b168
David Mazo <dmazo@biinyu.com.co>
2020-02-05 11:54:37 -0500

    table and contadores form created

--- src/app/pages/forms/contadores/contadores.component.ts
+++ src/app/pages/forms/contadores/contadores.component.ts
@@ -1,4 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ModuleWithProviders, ViewChild } from '@angular/core';
+import { Router, RouterModule, ActivatedRoute, Params, PreloadAllModules } from '@angular/router';
+import { UserService } from '../../../@core/mock/users.service';
+import { stringify } from 'querystring';
+import {MatTable} from '@angular/material';
+
+let ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Juan Pablo Ortiz', nit: '1047836647', address: 'cll 22 # 25-65', telephone: '6645794', email: 'juan@gmail.com'},
+  {position: 2, name: 'Ana Maria Barraza', nit: '93785548', address: 'cll 22 # 25-88', telephone: '6645700', email: 'info@gmail.com'},
+  /*
+  {position: 3, name: 'COMFENALCO', nit: '900824578', address: 'cll 22 # 25-65', telephone: '6645786', email: 'info@gmail.com'},
+  {position: 4, name: 'ARGOS', nit: '900824440', address: 'cll 22 # 25-65', telephone: '6645725', email: 'info@gmail.com'},
+  {position: 5, name: 'HOCOL', nit: '900824512', address: 'cll 22 # 25-65', telephone: '6645799', email: 'info@gmail.com'},
+  {position: 6, name: 'MEXICHEN', nit: '900635542', address: 'cll 22 # 25-65', telephone: '6439810', email: 'info@gmail.com'},
+  */
+];
+
+export interface PeriodicElement {
+  position: number;
+  name: string;
+  nit: string;
+  address: string;
+  telephone: string;  
+  email: string;
+}
 
 @Component({
   selector: 'ngx-contadores',
@@ -7,9 +31,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContadoresComponent implements OnInit {
 
-  constructor() { }
+  public title: string
+  public tableTitle: string
+  public name: string
+  public nit: string
+  public address: string
+  public telephone: string
+  public email: string
+
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: ActivatedRoute,
+    private _user_service: UserService
+    ) {
+      this.title = "Registrar Usuario"
+      this.tableTitle = "Contadores"
+      this.name = ""
+      this.nit = ""
+      this.address = ""
+      this.telephone = ""
+      this.email = ""
+    }
+
+  displayedColumns: string[] = ['position', 'name', 'nit', 'telephone'];
+  dataSource = ELEMENT_DATA;
+  @ViewChild(MatTable,null) table: MatTable<any>;
+
+  updateDataSource(){
+    ELEMENT_DATA.push({position: 8, name: this.name, nit: this.nit, address: this.address,  telephone: this.address, email: this.email});
+    console.log("ELEMENT_DATA")
+    console.log(ELEMENT_DATA)
+    console.log("datasource")
+    console.log(ELEMENT_DATA)
+    this.dataSource = ELEMENT_DATA;
+    console.log("")
+    console.log("")
+    this.table.renderRows();
+  }
 
   ngOnInit() {
   }
+
 
 }
