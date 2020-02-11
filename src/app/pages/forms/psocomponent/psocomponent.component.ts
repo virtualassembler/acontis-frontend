77b4430500c17ada46cf09d4a955f4fac590733a
David Mazo <dmazo@biinyu.com.co>
2020-02-10 22:13:39 -0500

    http-post-request-branch

--- src/app/pages/forms/psocomponent/psocomponent.component.ts
+++ src/app/pages/forms/psocomponent/psocomponent.component.ts
@@ -5,13 +5,13 @@ import { stringify } from 'querystring';
 import {MatTable} from '@angular/material';
 import { PsoService } from '../pso.service';
 import { IPso } from '../pso';
+import { HttpClient } from '@angular/common/http';
 
 /*
 let ELEMENT_DATA: PeriodicElement[] = [
   {position: 1, name: 'HUDC', nit: '900824578', address: 'cll 22 # 25-65', telephone: '6645711', email: 'info@gmail.com'},
   {position: 2, name: 'UNISINU', nit: '900824578', address: 'cll 22 # 25-65', telephone: '6645700', email: 'info@gmail.com'},
 ];
-
 
 export interface PeriodicElement {
   position: number;
@@ -39,12 +39,14 @@ export class PsocomponentComponent implements OnInit {
   public telephone: string
   public email: string
   public pso = [];
+  postData:{}
 
   constructor(
     private _route: ActivatedRoute,
     private _router: ActivatedRoute,
     private _user_service: UserService,
-    private _psoService: PsoService
+    private _psoService: PsoService,
+    private http:HttpClient
     ) {
       this.title = "Nueva empresa"
       this.tableTitle = "Empresas"
@@ -71,12 +73,17 @@ export class PsocomponentComponent implements OnInit {
     //this.dataSource = ELEMENT_DATA;
     //this.pso.push({position: 8, name: this.name, nit: this.nit, address: this.address,  telephone: this.address, email: this.email});
     //this.pso.push({id_company: 8, name_company: this.name, nit_company: this.nit, address_company: this.address,  telephone_company: this.address, email_company: this.email});
-    
     this.pso.push({id_company: 8, name_company: this.name, nit_company: this.nit, address_company: this.address,  telephone_company: this.address, email_company: this.email});
-    this.dataSource = this.pso;
+    console.log(this.name)
+    //this line cause datasource be just one element and erase the prevoius content
+    //this.dataSource = this.pso;
     this.table.renderRows();
-  }
+    this.postData={"name_company": this.name, "nit_company": this.nit, "address_company": this.address,  "telephone_company": this.address, "email_company": this.email};
+    this.http.post("http://localhost/acontis-backend/public/api/companies",this.postData).toPromise().then((data:any) =>{
+      console.log(data);
+      console.log(data);
 
-  
+    })
+  }
 
 }
