bedd1f7c17477173451bc2fea5a95d27b2631452
David Mazo <dmazo@biinyu.com.co>
2020-02-11 10:19:24 -0500

    http-get-contadores-module first commit

--- src/app/pages/forms/psocomponent/psocomponent.component.ts
+++ src/app/pages/forms/psocomponent/psocomponent.component.ts
@@ -7,22 +7,6 @@ import { PsoService } from '../pso.service';
 import { IPso } from '../pso';
 import { HttpClient } from '@angular/common/http';
 
-/*
-let ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'HUDC', nit: '900824578', address: 'cll 22 # 25-65', telephone: '6645711', email: 'info@gmail.com'},
-  {position: 2, name: 'UNISINU', nit: '900824578', address: 'cll 22 # 25-65', telephone: '6645700', email: 'info@gmail.com'},
-];
-
-export interface PeriodicElement {
-  position: number;
-  name: string;
-  nit: string;
-  address: string;
-  telephone: string;  
-  email: string;
-}
-*/
-
 @Component({
   selector: 'ngx-psocomponent',
   templateUrl: './psocomponent.component.html',
@@ -59,33 +43,21 @@ export class PsocomponentComponent implements OnInit {
 
   ngOnInit() {
     console.log("aag")
-    //console.log(JSON.stringify(this._psoService.getleagues()))
     this._psoService.getleagues().subscribe(data => this.dataSource = data)
   }
 
   displayedColumns: string[] = ['position', 'name', 'nit', 'telephone'];
-  //dataSource = ELEMENT_DATA;
   dataSource = this.pso;
   @ViewChild(MatTable,null) table: MatTable<IPso>;
 
   updateDataSource(){
-    //ELEMENT_DATA.push({position: 8, name: this.name, nit: this.nit, address: this.address,  telephone: this.address, email: this.email});
-    //this.dataSource = ELEMENT_DATA;
-    //this.pso.push({position: 8, name: this.name, nit: this.nit, address: this.address,  telephone: this.address, email: this.email});
-    //this.pso.push({id_company: 8, name_company: this.name, nit_company: this.nit, address_company: this.address,  telephone_company: this.address, email_company: this.email});
     this.pso.push({id_company: 8, name_company: this.name, nit_company: this.nit, address_company: this.address,  telephone_company: this.address, email_company: this.email});
     console.log(this.name)
-    //this line cause datasource be just one element and erase the prevoius content
-    //this.dataSource = this.pso;
     this.table.renderRows();
     this.postData={"name_company": this.name, "nit_company": this.nit, "address_company": this.address,  "telephone_company": this.address, "email_company": this.email};
     this.http.post("http://biinyugames.com/acontis/acontis-backend/public/api/companies",this.postData).toPromise().then((data:any) =>{
-      console.log(data);
-      console.log(data);
-      console.log(data);
-      //this.updateDataSource();
       this._psoService.getleagues().subscribe(data => this.dataSource = data)
     })
   }
-
+  
 }
