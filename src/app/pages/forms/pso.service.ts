bf659b90980e613bb13ae35e6f5de44768cc9670
David Mazo <dmazo@biinyu.com.co>
2020-02-10 19:48:40 -0500

    service conneted with interface within forms module

--- src/app/pages/forms/pso.service.ts
+++ src/app/pages/forms/pso.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IPso } from './pso';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PsoService {
 
-  constructor() { }
+  private _url :string = "http://localhost/acontis-backend/public/api/companies";
+
+  constructor(private http: HttpClient) { }
+  
+  getleagues(): Observable<IPso[]>{
+    return this.http.get<IPso[]>(this._url);
+  }
+
 }
